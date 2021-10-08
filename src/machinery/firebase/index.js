58bf3998f3e3f0f2ec9485ab240ac9a624dcd382
Abdullai Tahiru <42780120+abdullai-t@users.noreply.github.com>
2021-10-08 11:19:14 +0000

    firebase reintegration

--- src/machinery/firebase/index.js
+++ src/machinery/firebase/index.js
@@ -1,15 +1,22 @@
 import firebase from 'firebase/app'
 import 'firebase/storage'
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyBxlLnETZNnlHTsrRAL4cFGN1UE3ly6HdQ",
-    authDomain: "luxcom-hotel.firebaseapp.com",
-    projectId: "luxcom-hotel",
-    storageBucket: "luxcom-hotel.appspot.com",
-    messagingSenderId: "739411191867",
-    appId: "1:739411191867:web:e9b88a5396502f100dd41e",
-    measurementId: "G-62KFRPQWSW"
-  };
+const firebaseConfig = {
+
+  apiKey: "AIzaSyCLK-4amHIIpnpSk5phh_FZDr4-zu1JBog",
+
+  authDomain: "luxcom-hotel-admin.firebaseapp.com",
+
+  projectId: "luxcom-hotel-admin",
+
+  storageBucket: "luxcom-hotel-admin.appspot.com",
+
+  messagingSenderId: "516912338451",
+
+  appId: "1:516912338451:web:1ac2ed7a62714ebcc6d1e6"
+
+};
+
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
